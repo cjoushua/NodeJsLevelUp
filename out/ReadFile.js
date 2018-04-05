ad41356af833e88033c1bf1ea2f22ee3356c4a62
cjoushua@gmail.com <cjoushua@gmail.com>
2018-04-05 17:29:52 +0800

    del out

--- out/ReadFile.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var contents = fs.readFileSync('src/VSCodeRightClick.txt', 'utf8');
-console.log(contents);
-//# sourceMappingURL=ReadFile.js.map
--- out/callServer.js
+++
@@ -1,4 +0,0 @@
-var server = require('./Server');
-var router = require("./router");
-server.start(router.route);
-//# sourceMappingURL=callServer.js.map
