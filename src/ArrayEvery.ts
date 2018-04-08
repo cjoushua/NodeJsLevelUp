94276308128f08661dcec24766c143c508d22978
cjoushua@gmail.com <cjoushua@gmail.com>
2018-04-08 09:41:37 +0800

    modify ArrayeEvery

---
+++ src/ArrayEvery.ts
@@ -0,0 +1,13 @@
+//every() 方法會測試陣列中的所有元素是否都通過了由給定之函式所實作的測試。
+var ueridarr = ['1', '2', '3', '4', 'U206d25c2ea6bd87c17655609a1c37cb8'],
+    newArr = [];
+var re = "^U[0-9a-f]{32}$";
+
+
+  
+  console.log(ueridarr.every((guid) :any=>{guid.match(re);}));
+
+
+
+
+
