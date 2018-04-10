84e199912f13b4a8e6bf8fa2ff8449cbc8fca8db
cjoushua@gmail.com <cjoushua@gmail.com>
2018-04-11 07:35:39 +0800

    modify PromiseThen

--- src/PromiseThen.ts
+++ src/PromiseThen.ts
@@ -1,40 +1,17 @@
-
-interface icallApi{
-    doCall();
-}
-
-class callApi implements icallApi{
-    isSuccess:string;
-    constructor(postData:string){
-        this.isSuccess = "isSuccess";
-    }
-
-    callfailed() :string {
-        return "doCallApiFailed()";
-    }
-
-    doCall(){
-        return "doCallApi()";
-    }
-}
-
 var request = require('request');
+
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
   }
 })
 
-let postData = "goGoogleApi";
 
-var callGoogleApi = new callApi(postData);
-console.log(postData);
 
-console.log(callGoogleApi.isSuccess);
 
-console.log(callGoogleApi.callfailed());
 
-console.log(callGoogleApi.doCall());
+
+
 
 
 
