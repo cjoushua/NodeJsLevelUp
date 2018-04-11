41c2efb53c081e14f22ac66e113715ff0c9e905e
cjoushua@gmail.com <cjoushua@gmail.com>
2018-04-11 08:25:19 +0800

    add moment note

--- src/moment.ts
+++ src/moment.ts
@@ -9,6 +9,7 @@ var ed=new Date("2018-04-11 20:15" );
 
 let hourST = moment(sd);
 let hourED = moment(ed);
+//moment的unix()函數,這個函數用於取unix()時間差來計算日期相差多久。
 let workhour = ((hourED.unix() - hourST.unix()))/3600; 
 
 
